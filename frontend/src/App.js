02552e44d572d7be10c62037cbbe68220f6c7412
Jup1terrubb1t <sakam0r1sa@gmail.com>
2025-11-13 10:39:47 +0900

    TOP画面

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { Button, Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import Category from './components/Category';//カテゴリ一覧
-import AdminLoginPage from "./pages/AdminLoginPage.jsx";//管理者ログイン
-import AdminDashboardPage from "./pages/AdminDashboardPage.jsx";//管理者ダッシュボード
-import AdminCategoriesPage from "./pages/AdminCategoriesPage.jsx";//管理者カテゴリ管理
-import { AuthProvider } from "./auth/AuthContext"; //認証Context
-import PrivateRoute from "./auth/PrivateRoute"; //ログイン必須のルート
-import { useCategories } from "./hooks/useCategories";//カテゴリ一覧取得
-
+import Category from './components/Category';                     // TOPカテゴリ一覧
+import AdminLoginPage from "./pages/AdminLoginPage.jsx";          // 管理者ログイン
+import AdminDashboardPage from "./pages/AdminDashboardPage.jsx";  // 管理者ダッシュボード
+import AdminCategoriesPage from "./pages/AdminCategoriesPage.jsx";// 管理者カテゴリ管理
+import AdminPostsPage from "./pages/AdminPostsPage.jsx";          // 管理者投稿一覧
+import PostFormPage from "./pages/PostFormPage.jsx";              // 投稿フォーム
+import PostsPage from "./pages/PostsPage.jsx";                    // 投稿一覧
+import { AuthProvider } from "./auth/AuthContext";                // 認証Context
+import PrivateRoute from "./auth/PrivateRoute";                   // ログイン必須のルート
+import { useCategories } from "./hooks/useCategories";            // カテゴリ一覧取得
 
 // どのURLの時にどの画面を表示させるか(Reactアプリで最初に呼ばれるところ)
 const App = () => {
@@ -15,9 +17,12 @@ const App = () => {
     <AuthProvider>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/new" element={<PostFormPage />} /> 
         <Route path="/admin/login" element={<AdminLoginPage />} />
         <Route path="/admin/dashboard" element={<PrivateRoute><AdminDashboardPage /></PrivateRoute>}/>
         <Route path="/admin/categories" element={<PrivateRoute><AdminCategoriesPage /></PrivateRoute>}/>
+        <Route path="/admin/posts" element={<PrivateRoute><AdminPostsPage /></PrivateRoute>}/>
       </Routes>
     </AuthProvider>
   );
@@ -46,7 +51,7 @@ const  HomePage = () => {
           </Box>
           <CheckboxGroup>
             {categories.map((cat) => (
-              <Category key={cat.id} id={cat.id} name={cat.name} />
+              <Category key={cat.id} id={cat.id} name={cat.name} onClick={() => navigate(`/posts?category_id=${encodeURIComponent(cat.id)}`)}/>
             ))}
           </CheckboxGroup>
         </Box>
